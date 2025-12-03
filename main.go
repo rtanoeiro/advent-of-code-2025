cecf00c7a9c519cc714b41c88a4822c6acdd8e20
rtanoeiro <ramontanoeiro@gmail.com>
2025-12-03 22:51:18 +1100

    updated main

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"aoc-2025/day01"
+	"aoc-2025/day03"
 	"log"
 )
 
 // Each day results can be resolved by calling dayxx.Resolve()
 func main() {
-	result := day01.Resolve()
+	result := day03.Resolve()
 	log.Printf("Result for the current day is: %s", result)
 }
